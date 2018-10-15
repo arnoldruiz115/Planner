e1912b46108c85a820e1af190c58655f00a56ed4
Arnold Ruiz <arnoldruiz115@csu.fullerton.edu>
2018-10-14 18:03:23 -0700

    Included SQlite3

--- Planner/main.cpp
+++ Planner/main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
+#include "sqlite3.h"
 using std::cout;
 
 int main() {
 
+	sqlite3 *plannerDB;
+
 	cout << "This is my planner. See TO-DO list.";
 	system("pause");
+
+	return 0;
 }
